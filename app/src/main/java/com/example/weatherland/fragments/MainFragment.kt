9cb6217d77dbc2c5ecce283946a2d0218d60d171
Alexey Stepanov <alex2002693@gmail.com>
2023-12-16 14:55:24 +0400

    commit

--- app/src/main/java/com/example/weatherland/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherland/fragments/MainFragment.kt
@@ -37,7 +37,7 @@ import com.google.android.material.tabs.TabLayoutMediator
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
 
-const val API = "Ваш API-ключ с сайта"
+const val API = "API-ключ с сайта https://www.weatherapi.com/"
 
 class MainFragment : Fragment() {
     private lateinit var fLocationClient : FusedLocationProviderClient
